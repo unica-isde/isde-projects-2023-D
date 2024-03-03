dd03a7e94f64d3f65f0b7373fcc6ea07cbb4d8a0
njehm <njehmed7@gmail.com>
2024-03-03 23:05:44 +0100

    Added OutputPNG API -- Made some refactoring to the rest of the code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Dict, List
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -12,6 +13,7 @@ from app.config import Configuration
 from app.forms.classification_form import ClassificationForm
 from app.ml.classification_utils import classify_image
 from app.utils import list_images
+import matplotlib.pyplot as plt
 
 
 app = FastAPI()
@@ -47,13 +49,18 @@ def create_classify(request: Request):
 
 @app.post("/classifications")
 async def request_classification(request: Request):
+    folder_path = "app/static/output/json/"
+    # Check if the folder exists
+    if not os.path.exists(folder_path):
+        # If it doesn't exist, create it
+        os.makedirs(folder_path)
     form = ClassificationForm(request)
     await form.load_data()
     image_id = form.image_id
     model_id = form.model_id
     classification_scores = classify_image(model_id=model_id, img_id=image_id)
     out = json.dumps(classification_scores)
-    with open("out.json", "w") as outfile:
+    with open("app/static/output/json/out.json", "w") as outfile:
         outfile.write(out)
     return templates.TemplateResponse(
         "classification_output.html",
@@ -65,5 +72,28 @@ async def request_classification(request: Request):
     )
 # Download JSON file containing prediction output
 @app.get("/outputJSON")
-def outputJSON():
+def output_json():
     return FileResponse(path="out.json", filename="out.json", media_type='text/json')
+
+
+# Download Image file containing plot
+@app.get("/outputPNG")
+def output_png():
+    folder_path = "app/static/output/png/"
+    # Check if the folder exists
+    if not os.path.exists(folder_path):
+        # If it doesn't exist, create it
+        os.makedirs(folder_path)
+    with open("app/static/output/json/out.json") as json_file:
+        data = json.load(json_file)
+        x = [item[0] for item in data]
+        y = [item[1] for item in data]
+        plt.barh(x, y)
+        # setting label of y-axis
+        plt.ylabel("Y")
+        # setting label of x-axis
+        plt.xlabel("X")
+        plt.title("Prediction")
+        plt.savefig('app/static/output/png/img.png')
+        plt.clf()
+        return FileResponse(path="app/static/output/png/img.png", filename="img.png", media_type='image/png')
